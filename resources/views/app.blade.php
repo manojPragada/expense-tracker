2ac27e3336614d50ce96e31481fd7ae4953f70b0
SaiManojPeopleTail <saimanoj@peopletail.com>
2026-01-03 21:20:39 -0500

    refining

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Fav Icon -->
+        <link rel="icon" href="{{ asset('favicon-exp.png') }}" type="image/png">
         <!-- Scripts -->
         @routes
         @viteReactRefresh
